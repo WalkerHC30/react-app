85f50614e3da96a85327b5cb521cd485a8cb2006
David_Chen_HT <vscode1005@hotaileasing.com.tw>
2024-09-16 10:32:09 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+function App() {
+  const [alertVisible, setVisiblity] = useState(false);
+  return (
+    <div>
+      {alertVisible && (
+        <Alert onClose={() => setVisiblity(false)}>
+          <span>My Alert</span>
+        </Alert>
+      )}
+      <Button color="secondary" onClick={() => setVisiblity(true)}>
+        Button
+      </Button>
+    </div>
+  );
+}
+
+export default App;
