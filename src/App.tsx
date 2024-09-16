77dab8403ee4f379f849ef02c06e2cd04a7cef7a
David_Chen_HT <vscode1005@hotaileasing.com.tw>
2024-09-16 14:11:34 +0800

    [update]useRef的使用

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,12 @@
 import { useEffect, useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
+
+import Form from "./components/Form";
 
 let count = 0;
 function App() {
-  const [alertVisible, setVisiblity] = useState(false);
-  count++;
-
-  console.log(count);
-
-  const array = ["jjj", "qqq"];
-  const newArr = array.map((item) => (item === "jjj" ? "JoJo" : item));
-  console.log(newArr);
-
-  // useEffect(() => {
-  //   console.log(count);
-  // }, []);
   return (
     <div>
-      {alertVisible && (
-        <Alert onClose={() => setVisiblity(false)}>
-          <span>My Alert</span>
-        </Alert>
-      )}
-      <Button color="secondary" onClick={() => setVisiblity(true)}>
-        BUTTON
-      </Button>
+      <Form />
     </div>
   );
 }
