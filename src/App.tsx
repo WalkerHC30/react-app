9e549303de4042f79473e7f3fa8067839faaf5c0
David_Chen_HT <vscode1005@hotaileasing.com.tw>
2024-09-16 13:25:46 +0800

    [update]更新筆記

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
+
+let count = 0;
 function App() {
   const [alertVisible, setVisiblity] = useState(false);
+  count++;
+
+  console.log(count);
+
+  const array = ["jjj", "qqq"];
+  const newArr = array.map((item) => (item === "jjj" ? "JoJo" : item));
+  console.log(newArr);
+
+  // useEffect(() => {
+  //   console.log(count);
+  // }, []);
   return (
     <div>
       {alertVisible && (
